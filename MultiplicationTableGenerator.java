2211799a238a31d9652e33b07284009ecf29967d
Heldo <111704946+HeldoT@users.noreply.github.com>
2025-04-21 17:51:02 -0700

    Add files via upload
    A simple multiplication table generator.

---
+++ MultiplicationTableGenerator.java
@@ -0,0 +1,26 @@
+package day6;
+
+import java.util.Scanner;
+
+public class MultiplicationTableGenerator {
+
+	public static void main(String[] args) {
+
+		Scanner scanner =  new Scanner(System.in);
+		
+		System.out.println("Please enter a number to print in it's multiplication table:" );
+		
+		int number = scanner.nextInt();
+		
+		System.out.println("Multiplication Table for " + number + ":");
+		
+		for (int i = 1; i <= 10; i = i + 1)
+		{
+			int result = number * i;
+			//System.out.println(result);
+			System.out.println(number + " x " + i + "=" + result);
+		}
+		
+	}
+
+}
